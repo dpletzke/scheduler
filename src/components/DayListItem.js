dd22b70f0c3f9a5018dd14e221ca8d9dd34a24fb
daniel <dpletzke@gmail.com>
2020-10-26 20:38:56 -0500

    create stories for daylist

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,6 +2,11 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require('classnames');
 
+function formatSpots(spots) {
+  return (spots ? spots : 'no') + (spots !== 1 ? ' spots' : ' spot') + ' remaining';
+} 
+
+
 export default function DayListItem(props) {
 
   const itemClass = classNames('day-list__item', {
@@ -11,7 +16,7 @@ export default function DayListItem(props) {
   return (
     <li className={itemClass} onClick={props.setDay}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">Spots Left: {props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
