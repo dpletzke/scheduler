5daa4b621a3e9ce5c60b01a2ab2ba857207baa15
daniel <dpletzke@gmail.com>
2020-10-26 21:31:29 -0500

    implement component and stories

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,11 +6,7 @@ const classNames = require('classnames');
 
 export default function DayList(props) {
   const { days, day, setDay } = props;
-  // const itemClass = classNames('day-list__item', {
-  //   'day-list__item--selected': props.selected,
-  //   'day-list__item--full': !props.spots
-  // });
-  
+   
   const renderDays = days.map(({ id, name, spots }) => {
     const selected = day === name;
     const passProps = {
@@ -21,7 +17,7 @@ export default function DayList(props) {
       spots
     }
     return (<DayListItem {...passProps} />)
-  })  
+  })
   
 
   return (
