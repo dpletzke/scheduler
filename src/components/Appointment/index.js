78d25fa928cb3c308ff62cd11d8fbd20addfb8cc
daniel <dpletzke@gmail.com>
2020-10-28 00:45:32 -0500

    work on render all appointment subcomponents together

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,41 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  const { time } = props;
+  const {
+    time,
+    student,
+    interview,
+    onEdit,
+    onDelete, 
+    onAdd 
+  } = props;
 
-  const passProps = {
+  const headerProps = {
     time
+  }
+
+  let showProps = {};
+  if (interview) {
+    showProps = {
+      student,
+      interviewer : interview.interviewer,
+      onEdit,
+      onDelete
+    }
+  };
+  
+  const emptyProps = {
+    onAdd,
   }
 
   return (
     <article className="appointment">
-      <Header {...passProps} />
+      <Header {...headerProps} />
+      {interview ? <Show {...showProps} /> : <Empty {...emptyProps} />}
     </article>
   );
 }
