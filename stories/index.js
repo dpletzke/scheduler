853f6c99c186b3150da7829f674dc5532ad3ec45
daniel <dpletzke@gmail.com>
2020-10-27 20:41:47 -0500

    implement show with stories and props, make empty run passed func

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 storiesOf("Button", module)
@@ -139,6 +140,7 @@ storiesOf("Appointment", module)
   .add("Appointment w/ Time", () => <Appointment time='12pm' />)
   .add("Header", () => <Header time='12pm' />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student='Steven Santucchi' interviewer={interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')}/>)
 
 
 
