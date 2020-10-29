30e15fc1d4acec3b6c78493cdd8a3cef28e16005
daniel <dpletzke@gmail.com>
2020-10-28 22:38:58 -0500

    implement and test custom hook for appointment mode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+export default function useVisualMode (initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  const back = () => { 
+    if (mode !== initialMode) history.pop();
+    setMode(history[history.length - 1]);
+   };
+
+  const transition = (newMode, replace = false) => {
+    if (replace) history.pop();
+    setHistory(prev => [...prev, newMode]);
+    setMode(newMode);
+  }
+
+  return {mode, back,  transition};
+}
