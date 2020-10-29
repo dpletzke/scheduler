b99e2698d470a6898a47192e6060a5e9ef104ff3
daniel <dpletzke@gmail.com>
2020-10-28 21:42:39 -0500

    write interview getter function and tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -10,3 +10,18 @@ export function getAppointmentsForDay(state, day) {
     return  appointmentData.filter(({ id }) => today.appointments.includes(id));
   }
 }
+
+
+
+export function getInterview(state, interview) {
+  const { interviewers } = state;
+  
+  if(!interview) {
+    return null;
+  } else {
+    const interviewer = interviewers[interview.interviewer];
+    return {...interview, interviewer }
+  }
+  }
+
+
