9be99f0d0e8318decff49a7d7975778bddbe35db
daniel <dpletzke@gmail.com>
2020-10-29 19:05:43 -0500

    create helper to get interviewers doing a interview for that day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -7,11 +7,9 @@ export function getAppointmentsForDay(state, day) {
     return [];
   } else {
     const appointmentData = Object.values(appointments);
-    return  appointmentData.filter(({ id }) => today.appointments.includes(id));
+    return appointmentData.filter(({ id }) => today.appointments.includes(id));
   }
 }
-
-
 
 export function getInterview(state, interview) {
   const { interviewers } = state;
@@ -22,6 +20,26 @@ export function getInterview(state, interview) {
     const interviewer = interviewers[interview.interviewer];
     return {...interview, interviewer }
   }
+}
+
+export function getInterviewersForDay(state, day) {
+  const { days, interviewers } = state;
+
+  const today = days.find(d => d.name === day);
+
+  if (!today || !today.appointments) {
+    return [];
+  } else {
+
+    const todaysInterviewerIds = getAppointmentsForDay(state, day)
+        .filter(({ interview }) => interview)
+        .map(({ interview }) => interview.interviewer);
+
+    const todaysInterviewers = Object.values(interviewers)
+        .filter(({ id }) => todaysInterviewerIds.includes(id));
+
+    return todaysInterviewers;
   }
+}
 
 
